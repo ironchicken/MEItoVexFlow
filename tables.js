03ebe9c0678ff000c762203bb98860b7880f64ef
Raffaele Viglianti <raffaeleviglianti@gmail.com>
2012-02-03 16:23:41 +0000

    merged tables.js from experimental branch.

---
+++ tables.js
@@ -0,0 +1,47 @@
+// Component of MEItoVexFlow
+// Author: Raffaele Viglianti, 2012
+// 
+// Tables for MEI <-> VexFlow values
+
+mei2vexflowTables = {}
+
+mei2vexflowTables.positions = {
+  'above' : Vex.Flow.Modifier.Position.ABOVE,
+  'below' : Vex.Flow.Modifier.Position.BELOW
+}
+
+mei2vexflowTables.articulations = {
+  'acc': 'a>',
+  'stacc': 'a.',
+  'ten': 'a-',
+  'stacciss': 'av',
+  'marc': 'a^',
+  //'marc-stacc':
+  //'spicc':
+  //'doit':
+  //'rip':
+  //'plop':
+  //'fall':
+  //'bend':
+  //'flip':
+  //'smear':
+  'dnbow': 'am',
+  'upbow': 'a|',
+  //'harm':
+  'snap': 'ao',
+  //'fingernail':
+  //'ten-stacc':
+  //'damp':
+  //'dampall':
+  //'open':
+  //'stop':
+  //'dbltongue':
+  //'trpltongue':
+  //'heel':
+  //'toe':
+  //'tap':
+  'lhpizz': 'a+',
+  'dot': 'a.',
+  'stroke': 'a|'
+}
+
