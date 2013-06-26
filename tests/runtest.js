a969baca926171cb62ffc8e2a9711f33da83ddf5
Zoltan Komives <zoltan.komives@gmail.com>
2013-06-26 17:06:39 +0100

    new test case for single-measure scenario (incomplete)

--- tests/runtest.js
+++ tests/runtest.js
@@ -6,7 +6,11 @@ MEI2VF.RunTest = function(test_case, canvas, titleElem){
   //load the xml file...
   console.log("Running Test Case Title: '" + test_case.title + "' MEI-XML: '" + test_case.mei_xml + "'");
   xmlDoc=loadXMLDoc(test_case.mei_xml);
-  console.log('MEI-XML loaded.');
+  if (xmlDoc) { 
+    console.log('MEI-XML loaded.'); 
+  } else {
+    //TODO: throw exception
+  }
 
   //... and render it onto the canvas
   var MEI = xmlDoc.getElementsByTagNameNS("http://www.music-encoding.org/ns/mei", 'score');
