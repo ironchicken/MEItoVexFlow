c9fbaf9c97bd0815187c2f806818edcf77d1d30a
Zoltan Komives <zoltan.komives@gmail.com>
2013-06-26 13:54:23 +0100

    run arbitrary number of test cases

--- tests/runtest.js
+++ tests/runtest.js
@@ -1,13 +1,18 @@
 
-
 MEI2VF.RunTest = function(test_case, canvas, titleElem){
 
-  //load the appropriate file
+  $(titleElem).html(test_case.title);
+
+  //load the xml file...
+  console.log("Running Test Case Title: '" + test_case.title + "' MEI-XML: '" + test_case.mei_xml + "'");
   xmlDoc=loadXMLDoc(test_case.mei_xml);
-  
-  //render it onto the appropriate canvas
-	var MEI = xmlDoc.getElementsByTagNameNS("http://www.music-encoding.org/ns/mei", 'score');
-	MEI2VF.render_notation(MEI, canvas, 981,400);
-	$(titleElem).html(test_case.title);
+  console.log('MEI-XML loaded.');
+
+  //... and render it onto the canvas
+  var MEI = xmlDoc.getElementsByTagNameNS("http://www.music-encoding.org/ns/mei", 'score');
+  console.log('Rendering... ');
+  MEI2VF.render_notation(MEI, canvas, 981,400);
+  console.log('Done (' + test_case.title + ')');
 	
 }
+
