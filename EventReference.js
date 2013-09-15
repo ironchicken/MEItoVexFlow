9e975253aa86770280f3ad1bfd63528a47bd2df3
Richard Lewis <richard@rjlewis.me.uk>
2013-09-15 22:25:31 +0100

    Added copyright and licence notices

--- EventReference.js
+++ EventReference.js
@@ -8,6 +8,21 @@
 * holds the event is established. When the tstamp reference is being resolved, 
 * the xml:id is calculated using the generic function tstamp2id(), then the xml:id stored, 
 * thus marking that the reference is resolved.
+* 
+* Copyright © 2012, 2013 Richard Lewis, Raffaele Viglianti, Zoltan Komives,
+* University of Maryland
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); you
+* may not use this file except in compliance with the License.  You may
+* obtain a copy of the License at
+* 
+*    http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+* implied.  See the License for the specific language governing
+* permissions and limitations under the License.
 */
 
 
